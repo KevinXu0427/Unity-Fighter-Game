55b00283f5633407d692303fe056ff0d1d84cd9e
Zixuan Xu(Kevin) <48079771+KevinXu0427@users.noreply.github.com>
2020-10-30 21:07:14 -0700

    update
    -Added photonManager to control the connection
    -Added lobby menu for player to join the room
    -Added sample controller to make the movement

---
+++ Fighter_Game/Assets/Scripts/Movement.cs
@@ -0,0 +1,40 @@
+﻿using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Movement : MonoBehaviourPun
+{
+    [SerializeField] private float movementSpeed = 0.0f;
+
+    private CharacterController controller = null;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (photonView.IsMine)
+        {
+            TakeInput();
+        }
+    }
+
+    private void TakeInput()
+    {
+        // only move to left or right
+        Vector3 movement = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = 0f,
+        }.normalized;
+
+        controller.SimpleMove(movement * movementSpeed);
+
+    }
+}
---
+++ Fighter_Game/Assets/Scripts/NameInput.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+
+public class NameInput : MonoBehaviour
+{
+    [SerializeField] private InputField nameInputField = null;
+    [SerializeField] private Button continueButton = null;
+
+    private const string playerprefsNameKey = "PlayerName";
+
+    private void Start()
+    {
+        SetupInputField();
+    }
+
+    private void SetupInputField()
+    {
+        if (!PlayerPrefs.HasKey(playerprefsNameKey))
+        {
+            return;
+        }
+
+        string defaultName = PlayerPrefs.GetString(playerprefsNameKey);
+        nameInputField.text = defaultName;
+        SetPlayerName(defaultName);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        // only press button when you input someting.
+        continueButton.interactable = !string.IsNullOrEmpty(name);
+    }
+
+    public void SavePLayerName()
+    {
+        string playerName = nameInputField.text;
+        PhotonNetwork.NickName = playerName;
+
+        PlayerPrefs.SetString(playerprefsNameKey, playerName);
+    }
+}
